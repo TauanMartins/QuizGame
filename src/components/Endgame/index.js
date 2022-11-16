570f415a2408436ac0bbcef6c9a133e90572edf3
TauanMartins <TauanP.Martins@outlook.com>
2022-11-16 12:55:36 -0300

    Documentação, ajustes finos de funções e outros ques..

--- src/components/Endgame/index.js
+++ src/components/Endgame/index.js
@@ -7,12 +7,12 @@ import Score from "../Score";
 function Endgame({ endgame, visible }, ref) {
 
     const [open, setOpen] = useState(false);
-    const { pontos, name} = useContext(GlobalState);
+    const { pontos, name } = useContext(GlobalState);
     function handleTooltip() {
         setOpen(!open);
     }
     ref.current = {
-        endgame: function () {
+        endgame: function (pontos) {
             setOpen(true)
         },
         ...{ open: open }
@@ -21,56 +21,43 @@ function Endgame({ endgame, visible }, ref) {
         <Container fluid>
             <Modal className="endgame" size='lg' style={{ minWidth: '50%', minHeight: '10%' }} isOpen={open} >
                 <ModalBody>
-                    {visible ?
-                        <>
-                            <Col >
+                    <Col>
+                        <Row className="d-flex justify-content-center align-items-center">
+                            <h4 >
+                                <b>
+                                    Muito bom, {`${name}`}!
+                                </b>
+                                <br />
+                                <b>
+                                    Você concluiu o jogo!
+                                </b>
+                            </h4>
+                        </Row>
+                        <Row >
+                            <Col>
                                 <Row className="d-flex justify-content-center align-items-center">
-                                    <h4>
-                                        <b>
-                                            ScoreBoard
-                                        </b>
-                                    </h4>
+                                    {'Seu score total foi de '}
+                                    {Math.floor(pontos)}
+                                    {pontos === 1 ? ' ponto' : ' pontos'}.
+                                </Row>
+
+
+                                <Row className="d-flex justify-content-center align-items-center">
+                                    <b>
+                                        {
+                                            pontos >= 9 ? 'Parabéns, você sabe muito!' :
+                                                pontos >= 7 ? 'Ok, você sabe mais ou menos.' :
+                                                    pontos >= 3 ? 'Vish, por pouco você não é um burro' :
+                                                        pontos >= 0 ? 'Sinto muito!' : ''
+                                        }
+                                    </b>
+                                </Row>
+                                <Row className="d-flex justify-content-center align-items-center">
+                                    Confira sua posição e score abaixo:
                                 </Row>
                             </Col>
-                            <br />
-                        </>
-                        :
-                        <Col>
-                            <Row className="d-flex justify-content-center align-items-center">
-                                <h4 >
-                                    <b>
-                                        Muito bom, {`${name}`}!
-                                    </b>
-                                    <br />
-                                    <b>
-                                        Você concluiu o jogo!
-                                    </b>
-                                </h4>
-                            </Row>
-                            <Row >
-                                <Col>
-                                    <Row className="d-flex justify-content-center align-items-center">
-                                        Seu score total foi de <b>{' '+pontos+' '}</b> {pontos === 1 ? ' ponto' : ' pontos'}.
-                                    </Row>
-
-
-                                    <Row className="d-flex justify-content-center align-items-center">
-                                        <b>
-                                            {
-                                                pontos >= 9 ? 'Parabéns, você sabe muito!' :
-                                                    pontos >= 7 ? 'Ok, você sabe mais ou menos.' :
-                                                        pontos >= 3 ? 'Vish, por pouco você não é um burro' :
-                                                            pontos >= 0 ? 'Sinto muito!' : ''
-                                            }
-                                        </b>
-                                    </Row>
-                                    <Row className="d-flex justify-content-center align-items-center">
-                                        Confira sua posição e score abaixo:
-                                    </Row>
-                                </Col>
-                            </Row>
-                        </Col>
-                    }
+                        </Row>
+                    </Col>
                     <Col>
                         <Row className="justify-content-center">
                             <Score />
--- src/components/Question/index.js
+++ src/components/Question/index.js
@@ -6,18 +6,19 @@ import { shuffleArray } from "../DataComponents/RandomInt&ShuffledArray";
 function Question({ nothing }, ref) {
     const { questions, setQuestions, currentQuestion, setCurrentQuestion, setAnswers, setCorrectAnswer, setIMG } = useContext(GlobalState);
 
+    // função que baixa imagem se a questão possuir
     async function downloadImage(path) {
         try {
             getImage(path).then(response => {
                 const url = URL.createObjectURL(response.data)
                 setIMG(url)
             })
-
         } catch (error) {
             console.log('Error downloading image: ', error)
         }
     }
 
+    // função gera respostas de ordem aleatória para compor as alternativas
     function generateQuestion(question) {
         //console.log(question)
         setCorrectAnswer(question.rightAnswer)
@@ -28,25 +29,45 @@ function Question({ nothing }, ref) {
             return setAnswers(shuffleArray([question.rightAnswer, question.distractionAnswer1, question.distractionAnswer2, question.distractionAnswer3]))
         }
     }
+
+    // função reseta a questão e passa para a seguinte
     ref.current = {
         setList: function (allQuestions) {
-
-            // abaixo seta todas as questões da partida
+            // seta a lista de questões que serão perguntadas            
             setQuestions(allQuestions)
+
+            // seta a imagem como nula caso a questão anterior tivesse uma imagem
             setIMG(undefined)
-            // abaixo a primeira questão é selecionada para a questão
+
+            // abaixo a primeira questão é selecionada para a dispor na tela
             let firstQuestion = allQuestions[0]
             setCurrentQuestion(allQuestions[0])
-            // gera a primeira questão
+
+            // gera a ordem aleatória de respostas da questão atual
             generateQuestion(allQuestions[0])
+
+            // se o campo de imagem não for nulo ou indefinido 
+            // ele irá baixar a imagem para dispor na tela
             if(!(firstQuestion.img===null || firstQuestion.img===undefined)){
                 downloadImage(firstQuestion.img)
             }
         },
         nextQuestion: function () {
+            // função chamada para percorrer a lista de questões atual 
+            // abaixo ele verifica se nesse processo de chamada a última questão
+            // já está sendo exibida e se está encerra na hora para não haver erros
             var next_question = questions.indexOf(currentQuestion, 0) + 1;
+            var limit = questions.length;
+            if (next_question===limit) {
+                return "NoMoreQuestionsOver"
+            }
+            // gera as respostas de ordem aleatória da próxima questão
             generateQuestion(questions[next_question])
+
+            // seta a questão atual
             setCurrentQuestion(questions[next_question])
+
+            // baixa a imagem se a questão tiver uma
             if(!(questions[next_question].img===null || questions[next_question].img===undefined)){
                 downloadImage(questions[next_question].img)
             }else{
@@ -55,10 +76,8 @@ function Question({ nothing }, ref) {
 
         }
     }
-
     return (
         <h4><b>{currentQuestion.question}</b></h4>
     )
 }
-
 export default forwardRef(Question)
